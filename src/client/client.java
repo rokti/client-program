79adc22240c4019f8188001190b22a5fd91c8537
Rostislav <rostislav.hristov91@abv.bg>
2020-01-07 19:02:55 +0200

    Initial commit

---
+++ src/client/client.java
@@ -0,0 +1,16 @@
+package client;
+
+import java.net.*;
+import java.io.*;
+
+public class client {
+
+    public static void main(String[] args) throws IOException{
+        Socket s = new Socket("localhost",4999);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("hello");
+        pr.flush();
+        pr.close();
+    }
+}
